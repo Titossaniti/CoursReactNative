6f25f11382c5f0d1cc4e6a5df1fcc55f2923a2f6
Tristan Erard <tristan.erard@gmail.com>
2024-09-04 14:49:58 +0200

    ImagePicker added to replace the moving square

--- app/sensor.tsx
+++ app/sensor.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, Button } from 'react-native';
 import Animated, {
     useAnimatedStyle,
     useAnimatedSensor,
     SensorType,
     withSpring,
 } from 'react-native-reanimated';
+import * as ImagePicker from 'expo-image-picker';
+import {Link} from "@react-navigation/native";
 
 export default function sensor() {
     const gravity = useAnimatedSensor(SensorType.GRAVITY);
@@ -13,19 +15,36 @@ export default function sensor() {
     const animatedStyle = useAnimatedStyle(() => {
         return {
             transform: [
-                { translateX: withSpring(gravity.sensor.value.x * 15) },
-                { translateY: withSpring(gravity.sensor.value.y * 15) },
+                {translateX: withSpring(gravity.sensor.value.x * 15)},
+                {translateY: withSpring(gravity.sensor.value.y * 25)},
             ],
         };
     });
 
+    const [image, setImage] = useState<string | null>(null);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
+
+
     return (
         <View style={styles.container}>
-            <Animated.View style={[styles.box, animatedStyle]} />
+            <Link style={[styles.link]} to={'/'}>Retour accueil</Link>
+            <Button title="Choisir une image" onPress={pickImage}/>
+            {image && <Animated.Image source={{uri: image}} style={[styles.box, animatedStyle]}/>}
         </View>
     );
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -38,5 +57,16 @@ const styles = StyleSheet.create({
         width: 120,
         backgroundColor: '#353ee5',
         borderRadius: 20,
+        borderWidth: 2,
+        borderColor: 'rgba(240,240,240)',
     },
+    link: {
+        position: 'absolute',
+        top: 20,
+        padding: 10,
+        backgroundColor: '#f0f0f0',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
 });
